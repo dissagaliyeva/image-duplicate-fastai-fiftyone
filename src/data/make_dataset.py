39818cb50e9b33eef7932afa606aa9ccf9076495
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-05-08 15:30:56 +0400

    Add example downloader

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,41 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
+import shutil
+import splitfolders
+
+from simple_image_download import simple_image_download as simp
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+class ImageLoader:
+    def __init__(self, theme, path='data', count=100):
+        self.theme = theme
+        self.path  = path
+        self.count = count
+
+        self.sample_download()
+
+    def sample_download(self):
+        # print(os.listdir())
+        downloads = os.path.join(self.path, 'downloads')
+        path      = os.path.join(downloads, self.theme)
+
+        # create folders to store images
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+            os.mkdir(downloads)
+            os.mkdir(path)
+        if not os.path.exists(downloads):
+            os.mkdir(downloads)
+            os.mkdir(path)
+        if not os.path.exists(path):
+            os.mkdir(path)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+def get_title(title):
+    if '.' not in title:
+        return title.split('/')[-1]
+    return title.split('/')[-1].split('.')[0]
 
-    main()
+
+dataset = ImageLoader('frodo')
